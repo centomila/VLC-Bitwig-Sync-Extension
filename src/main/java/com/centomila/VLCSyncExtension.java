80dc0895786c14ebf59a48e9b3f0d8ab4b538fa1
centomila <26800547+centomila@users.noreply.github.com>
2024-09-08 06:04:37 +0200

    firset setting just in the gui

--- src/main/java/com/centomila/VLCSyncExtension.java
+++ src/main/java/com/centomila/VLCSyncExtension.java
@@ -3,66 +3,64 @@ package com.centomila;
 import com.bitwig.extension.controller.api.ControllerHost;
 import com.bitwig.extension.controller.api.Transport;
 import com.bitwig.extension.controller.ControllerExtension;
+import com.bitwig.extension.controller.api.Preferences;
+import com.bitwig.extension.controller.api.SettableBooleanValue;
+import com.bitwig.extension.controller.api.SettableStringValue;
 import com.centomila.VLCController;
 
-
-public class VLCSyncExtension extends ControllerExtension
-{
+public class VLCSyncExtension extends ControllerExtension {
    private Transport transport;
    private ControllerHost host;
    private boolean wasPlaying = false;
    private int currentPlayPosition = 0;
    private String seekVLC = "";
-   
-   protected VLCSyncExtension(final VLCSyncExtensionDefinition definition, final ControllerHost host)
-   {
+
+   protected VLCSyncExtension(final VLCSyncExtensionDefinition definition, final ControllerHost host) {
       super(definition, host);
    }
-   
+
    @Override
-   public void init()
-   {
+   public void init() {
       host = getHost();
+
       transport = host.createTransport();
-      
+      createSettingsObjects(host);
+
       // TODO: Perform your driver initialization here.
       host.showPopupNotification("VLCSync Initialized");
       transport.playPosition().markInterested();
       transport.playPositionInSeconds().markInterested();
       transport.getPosition().markInterested();
       transport.isPlaying().markInterested();
-      
+
       transport.playPosition().subscribe();
       transport.playPositionInSeconds().subscribe();
       transport.getPosition().subscribe();
       transport.isPlaying().subscribe();
-      
+
       // Add observer for play state changes
-      transport.isPlaying().addValueObserver(this::onPlayStateChanged);
-      
+      transport.isPlaying().addValueObserver(this::onPlayStateChangedVLCCommand);
+
    }
-   
+
    @Override
-   public void exit()
-   {
+   public void exit() {
       // TODO: Perform any cleanup once the driver exits
-      // For now just show a popup notification for verification that it is no longer running.
+      // For now just show a popup notification for verification that it is no longer
+      // running.
       getHost().showPopupNotification("VLCSync Exited");
    }
-   
+
    @Override
-   public void flush()
-   {
+   public void flush() {
       // TODO Send any updates you need here.
       currentPlayPosition = (int) transport.playPositionInSeconds().getAsDouble();
    }
-   
-   private void onPlayStateChanged(boolean isPlaying)
-   
+
+   private void onPlayStateChangedVLCCommand(boolean isPlaying)
    {
       final VLCController vlcController = new VLCController();
-      if (isPlaying && !wasPlaying)
-      {
+      if (isPlaying && !wasPlaying) {
          host.println("VLC Started");
          // send command to VLC using the VLCController class
          try {
@@ -76,10 +74,7 @@ public class VLCSyncExtension extends ControllerExtension
             e.printStackTrace();
          }
 
-
-      }
-      else if (!isPlaying && wasPlaying)
-      {
+      } else if (!isPlaying && wasPlaying) {
          host.println("VLC Stopped");
          // send command to VLC using the VLCController class
          try {
@@ -94,4 +89,13 @@ public class VLCSyncExtension extends ControllerExtension
       }
       wasPlaying = isPlaying;
    }
+
+   private void createSettingsObjects(final ControllerHost host) {
+      final Preferences preferences = host.getPreferences();
+      VLCIpString = preferences.getStringSetting("VLC IP or Host", "Controls", 100, "localhost");
+      VLCIpString.markInterested();
+
+   }
+      private SettableStringValue VLCIpString;
+
 }
